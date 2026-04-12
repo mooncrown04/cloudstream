bcf6b1768b4a1e7f94aa095e6f9318dae03dda49
mooncrown04 <mooncrown04@gmail.com>
2026-04-12 16:15:34 +0300

    Update GeneratorPlayer.kt

--- app/src/main/java/com/lagradost/cloudstream3/ui/player/GeneratorPlayer.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/player/GeneratorPlayer.kt
@@ -185,8 +185,12 @@ class GeneratorPlayer : FullScreenPlayer() {
 
     private var currentSelectedLink: Pair<ExtractorLink?, ExtractorUri?>? = null
     private var currentSelectedSubtitles: SubtitleData? = null
-    private var currentMeta: Any? = null
-    private var nextMeta: Any? = null
+    
+	//private var currentMeta: Any? = null
+    //yeni
+	override var currentMeta: Any? = null
+	//yeni
+	private var nextMeta: Any? = null
     private var isActive: Boolean = false
     private var isNextEpisode: Boolean = false // this is used to reset the watch time
 
