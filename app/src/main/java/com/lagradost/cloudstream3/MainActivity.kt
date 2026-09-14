6bb591817456ae3e354af71c5069463ab62c66f7
mooncrown04 <mooncrown04@gmail.com>
2026-09-14 15:16:34 +0300

    Update MainActivity.kt

--- app/src/main/java/com/lagradost/cloudstream3/MainActivity.kt
+++ app/src/main/java/com/lagradost/cloudstream3/MainActivity.kt
@@ -2079,25 +2079,31 @@ super.onCreate(savedInstanceState)
 
 
 
-
-//font
+// Font Overlay Uygulaması
 val fontValue = settingsManager.getString(getString(R.string.app_font_key), "Default") ?: "Default"
 
 val fontOverlayStyle = when (fontValue) {
     "ComicSans" -> R.style.ComicSansFontOverlay
+    "Consola" -> R.style.ConsolaFontOverlay
+    "Futura" -> R.style.FuturaFontOverlay
+    "GoogleSans" -> R.style.GoogleSansFontOverlay
     "Gotham" -> R.style.GothamFontOverlay
+    "LucidaGrande" -> R.style.LucidaGrandeFontOverlay
     "NetflixSans" -> R.style.NetflixSansFontOverlay
-    "Ubuntu" -> R.style.UbuntuFontOverlay
     "OpenSans" -> R.style.OpenSansFontOverlay
+    "Poppins" -> R.style.PoppinsFontOverlay
+    "ProductSans" -> R.style.ProductSansFontOverlay
+    "StixGeneral" -> R.style.StixGeneralFontOverlay
+    "TimesNewRoman" -> R.style.TimesNewRomanFontOverlay
+    "TrebuchetMs" -> R.style.TrebuchetMsFontOverlay
+    "Ubuntu" -> R.style.UbuntuFontOverlay
+    "Verdana" -> R.style.VerdanaFontOverlay
     else -> 0
 }
 
 if (fontOverlayStyle != 0) {
     theme.applyStyle(fontOverlayStyle, true)
 }
-
-
-
 
 
 
