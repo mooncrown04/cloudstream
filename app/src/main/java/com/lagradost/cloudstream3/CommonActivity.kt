eaf9c8874c5169a3a2ddb4338e43214d5f6534ce
mooncrown04 <mooncrown04@gmail.com>
2026-09-18 19:27:31 +0300

    Add new theme options for Galatasaray modes

--- app/src/main/java/com/lagradost/cloudstream3/CommonActivity.kt
+++ app/src/main/java/com/lagradost/cloudstream3/CommonActivity.kt
@@ -327,6 +327,7 @@ object CommonActivity {
                 "AmoledLight" -> R.style.AmoledModeLight
                 "Monet" -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
                     R.style.MonetMode else R.style.AppTheme
+				"GalatasarayBlue" -> R.style.GalatasarayBlueMode
                 "Galatasaray" -> R.style.GalatasarayMode // <-- BURAYA EKLENDİ
                 "GS" -> R.style.GSMode // <-- BURAYA EKLENDİ
                 "Dracula" -> R.style.DraculaMode
