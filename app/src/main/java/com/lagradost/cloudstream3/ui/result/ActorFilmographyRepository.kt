8d48e1a1b780d0c709c0deb396e69d4188fde0ce
mooncrown04 <mooncrown04@gmail.com>
2026-09-21 01:26:36 +0300

    Update ActorFilmographyRepository.kt

--- app/src/main/java/com/lagradost/cloudstream3/ui/result/ActorFilmographyRepository.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/result/ActorFilmographyRepository.kt
@@ -66,14 +66,14 @@ internal class ActorFilmographyRepository(
 
     suspend fun details(actor: Actor): ActorDetails? {
         val id = resolvePersonId(actor) ?: return null
-        return parseJson<ActorDetails>(request("/person/$id"))
+         return parseJson<ActorDetails>(request("/person/$id", emptyMap()))
 		//return parseJson<ActorDetails>(request("/person/$id", mapOf("language" to "en-US")))
     }
 
     suspend fun load(actor: Actor): List<SearchResponse> {
         val id = resolvePersonId(actor) ?: return emptyList()
         val credits = parseJson<TmdbCombinedCredits>(
-            request("/person/$id/combined_credits")
+            return parseJson<ActorDetails>(request("/person/$id", emptyMap()))
 			//request("/person/$id/combined_credits", mapOf("language" to "en-US"))
 			
         ).cast.orEmpty()
